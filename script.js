23f98dd862f804e203d9e962e9414691c5e1d53d
Mahdi Bozorgi <102772767+Mehtisup@users.noreply.github.com>
2023-02-06 07:21:41 +0330

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+<script>
+function sortFunction() {
+    var list, i, switching, b, shouldSwitch;
+
+    list = document.getElementById("list");
+    switching = true;
+
+    while (switching) {
+        switching = false;
+        b = list.getElementsByTagName("LI");
+
+        for (i = 0 ; i < (b.length - 1); i++) {
+            shouldSwitch = false;
+
+            if (b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()) {
+                shouldSwitch = true;
+                break;
+            }
+        }
+        if (shouldSwitch) {
+            b[i].parentNode.insertBefore(b[i + 1], b[i]);
+            switching = true;
+        }
+    }
+}
+
+</script>
